1ed6b010c551bf1128f6796cc914ed2817c44160
unknown <alwyyourbae@gmail.com>
2024-02-25 14:50:54 +1300

    feat: change font, add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,52 @@
+import Image from "@/node_modules/next/image";
+import Link from "@/node_modules/next/link";
+import { Rubik } from "next/font/google";
+
+export const rubik = Rubik({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+type Navlinks = {
+  url: string;
+  name: string;
+}[];
+
+const navlinks: Navlinks = [
+  {
+    url: "/",
+    name: "Home",
+  },
+  {
+    url: "/shop",
+    name: "Shop",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <header className="flex justify-between items-center px-5 box-border">
+      <Image
+        src="/Logo.svg"
+        alt="Waroengwidji Logo"
+        width={188}
+        height={53}
+        priority
+        className="pb-3"
+      />
+      <nav>
+        <ul className={`flex space-x-14 ${rubik.className} text-lg`}>
+          {navlinks.map((link) => (
+            <li key={link.name}>
+              <Link href={link.url} className='px-5 py-2 rounded-xl hover:text-green-50 hover:bg-amber-400 activer:bg-amber-400 focus:ring focus:ring-amber-200'>{link.name}</Link>
+            </li>
+          ))}
+          <li>
+            <Link href="/login" className='px-7 py-2 rounded-xl text-green-50 bg-green-700 focus:ring focus:ring-amber-200'>Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
